d64de598b955cc4f5b76a6225305610bc1143e2d
Ethan <ethan@nanar.com>
2022-01-26 18:04:23 -0800

    Working Sample

--- main.go
+++ main.go
@@ -3,34 +3,70 @@ package main
 import (
 	"fmt"
 	"math"
+	"math/rand"
+	"time"
 )
 
-func main() {
-	vertexSet := make([]int, 6)
-	vertexSet[0] = 0
-	vertexSet[1] = 5
-	vertexSet[2] = 1
-	vertexSet[3] = 4
-	vertexSet[4] = 2
-	vertexSet[5] = 3
+//This tells us if the labeling is actually a beta-labeling.
+func unique(intSlice []int) bool {
+	keys := make(map[int]bool)
+	list := []int{}
+	isUnique := true
+	for _, entry := range intSlice {
+		if _, value := keys[entry]; !value {
+			keys[entry] = true
+			list = append(list, entry)
+		} else {
+			isUnique = false
+		}
+	}
+	return isUnique
+}
 
-	fmt.Println("Vertices:", vertexSet)
-	edgeSet := make([][]int, len(vertexSet)-1)
+//This generates the vertex labels.
+func generateVertexLabels(numVertices int) []int {
+	rand.Seed(time.Now().Unix())
+	vertexSet := make([]int, numVertices)
+	vertexSet = rand.Perm(numVertices)
+	return vertexSet
+}
 
+//This generates the edge set but is specific to path graphs.
+func generateEdgeSet(numVertices int) [][]int {
+	edgeSet := make([][]int, numVertices-1)
 	for i := 0; i < len(edgeSet); i++ {
 		edgeSet[i] = make([]int, 2)
 		edgeSet[i][0] = i
 		edgeSet[i][1] = i + 1
 	}
+	return edgeSet
+}
 
-	fmt.Println("Edges:", edgeSet)
-
+//This generates the edge labels.
+func generateEdgeLabels(vertexSet []int, edgeSet [][]int) []int {
 	edgeLabels := make([]int, len(edgeSet))
-
 	for i := 0; i < len(edgeLabels); i++ {
 		edgeLabels[i] = int(math.Abs(float64(vertexSet[edgeSet[i][0]] - vertexSet[edgeSet[i][1]])))
 	}
+	return edgeLabels
+}
 
+//This is the main function.
+func main() {
+	var vertexSet []int
+	var edgeSet [][]int
+	var edgeLabels []int
+	numVertices := 6
+	for {
+		vertexSet = generateVertexLabels(numVertices)
+		edgeSet = generateEdgeSet(numVertices)
+		edgeLabels = generateEdgeLabels(vertexSet, edgeSet)
+		if unique(edgeLabels) == true {
+			break
+		}
+	}
+	fmt.Println("Vertices:", vertexSet)
+	fmt.Println("Edges:", edgeSet)
 	fmt.Println("Edge Labels:", edgeLabels)
 
 }
