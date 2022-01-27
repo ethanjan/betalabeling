8ba50c4fead7d39f4b8118bd0bda164b9d46d280
Ethan <ethan@nanar.com>
2022-01-27 09:42:52 -0800

    More Optimization

--- main.go
+++ main.go
@@ -43,12 +43,6 @@ func main() {
 
 	rand.Seed(time.Now().Unix())
 	vertexSet := make([]int, numVertices)
-	n := rand.Intn(numVertices)
-	vertexSet[n] = numVertices - 1
-
-	fmt.Println(n)
-
-	//theTruth := false
 
 	edgeSet := make([][]int, numVertices-1)
 
@@ -66,32 +60,39 @@ func main() {
 	table[3] = []int{2, 4}
 	table[4] = []int{3, 5}
 	table[5] = []int{4}
-
-	forbiddenIndices := make([]int, 0)
-
-	magicNumber := table[n][rand.Intn(len(table[n]))]
-
-	vertexSet[magicNumber] = 0
-
-	forbiddenIndices = append(forbiddenIndices, n)
-	forbiddenIndices = append(forbiddenIndices, magicNumber)
-
-	allowedNumbers := []int{1, 2, 3, 4}
-
-	fmt.Println(forbiddenIndices)
-
-	for i, _ := range vertexSet {
-		if !contains(forbiddenIndices, i) {
-			numB := rand.Intn(len(allowedNumbers))
-			vertexSet[i] = allowedNumbers[numB]
-			allowedNumbers = RemoveIndex(allowedNumbers, numB)
-			forbiddenIndices = append(forbiddenIndices, i)
-		}
-	}
-
 	edgeLabels := make([]int, len(edgeSet))
-	for i := 0; i < len(edgeLabels); i++ {
-		edgeLabels[i] = int(math.Abs(float64(vertexSet[edgeSet[i][0]] - vertexSet[edgeSet[i][1]])))
+	for {
+		n := rand.Intn(numVertices)
+		vertexSet[n] = numVertices - 1
+
+		forbiddenIndices := make([]int, 0)
+
+		magicNumber := table[n][rand.Intn(len(table[n]))]
+
+		vertexSet[magicNumber] = 0
+
+		forbiddenIndices = append(forbiddenIndices, n)
+		forbiddenIndices = append(forbiddenIndices, magicNumber)
+
+		allowedNumbers := []int{1, 2, 3, 4}
+
+		//fmt.Println(forbiddenIndices)
+
+		for i, _ := range vertexSet {
+			if !contains(forbiddenIndices, i) {
+				numB := rand.Intn(len(allowedNumbers))
+				vertexSet[i] = allowedNumbers[numB]
+				allowedNumbers = RemoveIndex(allowedNumbers, numB)
+				forbiddenIndices = append(forbiddenIndices, i)
+			}
+		}
+
+		for i := 0; i < len(edgeLabels); i++ {
+			edgeLabels[i] = int(math.Abs(float64(vertexSet[edgeSet[i][0]] - vertexSet[edgeSet[i][1]])))
+		}
+		if unique(edgeLabels) {
+			break
+		}
 	}
 
 	fmt.Println("Vertices:", vertexSet)
